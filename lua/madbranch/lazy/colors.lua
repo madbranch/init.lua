91f5570bad43c1a51d7d9af70df7cdc8c235677a
Adam Labranche <adam@labranche.co>
2025-01-02 07:01:44 -0500

    Tweak colors

--- lua/madbranch/lazy/colors.lua
+++ lua/madbranch/lazy/colors.lua
@@ -1,9 +1,9 @@
 return {
-  "Mofiqul/dracula.nvim",
+  "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-    vim.cmd("colorscheme dracula")
+    vim.cmd("colorscheme tokyonight-night")
   end
 }
